0569a17feb44fb5858158cacfd6c6ff2da1baf3d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-29 17:26:07 +0300

    Call api from duration page on button click

--- src/FrontEnd/stroitrendtask/src/components/Duration.tsx
+++ src/FrontEnd/stroitrendtask/src/components/Duration.tsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
 import { Base } from "./Base";
+import { BaseUrl } from "../utils/ApiUrl";
+import axios from "axios";
+import { Request } from "../models/Request";
+
+interface DurationData {
+  agents_chatting_duration: number;
+  count: number;
+  duration: number;
+}
+
+interface Record {
+  [date: string]: DurationData;
+}
+
+interface Duration {
+  name: string;
+  request: Request;
+  total: number;
+  records: Record;
+}
 
 export const Duration = () => {
+  const [duration, setDuration] = useState<Duration>();
+
+  const handleClick = async () => {
+    const { data } = await axios.get<Duration>(`${BaseUrl}/duration`);
+
+    setDuration(data);
+    console.table(data?.request);
+    console.table(data?.records);
+  };
+
   return (
     <Base>
-      <>Duration Page</>
+      <h1>Duration Page</h1>
+      <button onClick={handleClick}>Get Duration</button>
+
+      <p>Name: {duration?.name}</p>
+      <p>Total: {duration?.total}</p>
     </Base>
   );
 };
