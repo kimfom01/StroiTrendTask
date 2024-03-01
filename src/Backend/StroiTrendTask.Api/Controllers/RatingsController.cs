2421969265f646027793321babec59fa05b0102f
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-02 01:08:48 +0300

    Implement ratings controller logic

--- src/Backend/StroiTrendTask.Api/Controllers/RatingsController.cs
+++ src/Backend/StroiTrendTask.Api/Controllers/RatingsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using StroiTrendTask.Api.Models;
+using StroiTrendTask.Api.Services;
 
 namespace StroiTrendTask.Api.Controllers;
 
@@ -6,5 +8,37 @@ namespace StroiTrendTask.Api.Controllers;
 [Route("api/[controller]")]
 public class RatingsController : ControllerBase
 {
+    private readonly IRatingService _ratingService;
+
+    public RatingsController(IRatingService ratingService)
+    {
+        _ratingService = ratingService;
+    }
     
+    /// <summary>
+    /// Gets the ratings data.
+    /// </summary>
+    /// <returns>A json response with ratings data.</returns>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     GET /api/ratings
+    ///     
+    /// </remarks>
+    /// <response code="200">Returns a json response with ratings data</response>
+    /// <response code="404">If the resource could not be found</response>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Ratings>> GetRatings()
+    {
+        var ratings = await _ratingService.GetData();
+
+        if (ratings is null)
+        {
+            return NotFound("Ratings could not be found");
+        }
+
+        return Ok(ratings);
+    }
 }
