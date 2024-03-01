3009bfc55589de89517764763620a0d2e5da7952
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-02 00:55:28 +0300

    Implement logic for total chats endpoint

--- src/Backend/StroiTrendTask.Api/Controllers/TotalChatsController.cs
+++ src/Backend/StroiTrendTask.Api/Controllers/TotalChatsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using StroiTrendTask.Api.Models;
+using StroiTrendTask.Api.Services;
 
 namespace StroiTrendTask.Api.Controllers;
 
@@ -6,5 +8,37 @@ namespace StroiTrendTask.Api.Controllers;
 [Route("api/[controller]")]
 public class TotalChatsController : ControllerBase
 {
+    private readonly ITotalChatsService _totalChatsService;
+
+    public TotalChatsController(ITotalChatsService totalChatsService)
+    {
+        _totalChatsService = totalChatsService;
+    }
     
+    /// <summary>
+    /// Gets the total chats data.
+    /// </summary>
+    /// <returns>A json response with total chats data.</returns>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     GET /api/totalchats
+    ///     
+    /// </remarks>
+    /// <response code="200">Returns a json response with total chats data</response>
+    /// <response code="404">If the resource could not be found</response>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<TotalChats>> GetTotalChats()
+    {
+        var totalChats = await _totalChatsService.GetData();
+
+        if (totalChats is null)
+        {
+            return NotFound("Total chats could not be found");
+        }
+
+        return Ok(totalChats);
+    }
 }
