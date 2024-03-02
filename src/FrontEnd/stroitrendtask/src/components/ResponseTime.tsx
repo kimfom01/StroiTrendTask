06a4c5ea3a9d1fcb0ce04147e53e3b75fff8a9cc
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-02 11:26:17 +0300

    Implement response time page

--- src/FrontEnd/stroitrendtask/src/components/ResponseTime.tsx
+++ src/FrontEnd/stroitrendtask/src/components/ResponseTime.tsx
@@ -1,25 +1,88 @@
 import { Base } from "./Base";
+import { BaseUrl } from "../utils/ApiUrl";
+import { Request } from "../models/Request";
+import axios from "axios";
+import "./Table.css";
+import { useState } from "react";
+import { RequestTable } from "./RequestTable";
 
-// interface ResponseTimeData {
-//   count: number;
-//   response_time: number;
-// }
+interface ResponseTimeData {
+  count: number;
+  response_time: number;
+}
 
-// interface Record {
-//   [date: string]: ResponseTimeData;
-// }
+interface Record {
+  [date: string]: ResponseTimeData;
+}
 
-// interface ResponseTimeReport {
-//   name: string;
-//   request: Request;
-//   total: number;
-//   records: Record;
-// }
+interface ResponseTimeReport {
+  name?: string;
+  request?: Request;
+  total?: number;
+  records?: Record;
+}
+
+const Table = ({ records }: ResponseTimeReport) => {
+  return (
+    <div className="table-root">
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Count</th>
+            <th>Response Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {records &&
+            Object.entries(records).map(
+              ([date, { count, response_time }], key) => {
+                return (
+                  <tr key={key}>
+                    <td>{date && new Date(date).toLocaleString()}</td>
+                    <td>{count}</td>
+                    <td>{response_time}</td>
+                  </tr>
+                );
+              }
+            )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export const ResponseTime = () => {
+  const [responseTimeReport, setResponseTimeReport] =
+    useState<ResponseTimeReport>();
+
+  const handleClick = async () => {
+    const { data } = await axios.get<ResponseTimeReport>(
+      `${BaseUrl}/responsetime`
+    );
+
+    setResponseTimeReport(data);
+  };
   return (
     <Base>
-      <>Response Time Page</>;
+      <h1>Response Time Page</h1>
+      <button onClick={handleClick}>Get Response Time</button>
+      <div>
+        <h2>Name: {responseTimeReport?.name}</h2>
+        <h2>Total: {responseTimeReport?.total}</h2>
+        <h5>
+          Filter Group Values:{" "}
+          {responseTimeReport?.request?.filters?.groups.values.join(", ")}
+        </h5>
+      </div>
+      <div className="tables">
+        {responseTimeReport?.request && (
+          <RequestTable request={responseTimeReport?.request} />
+        )}
+        {responseTimeReport?.records && (
+          <Table records={responseTimeReport?.records} />
+        )}
+      </div>
     </Base>
   );
 };
