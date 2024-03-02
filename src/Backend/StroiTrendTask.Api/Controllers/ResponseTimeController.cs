2c47afc6c535eda162d85bba254dd7839153d332
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-02 11:18:17 +0300

    Add logic to respose time controller

--- src/Backend/StroiTrendTask.Api/Controllers/ResponseTimeController.cs
+++ src/Backend/StroiTrendTask.Api/Controllers/ResponseTimeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using StroiTrendTask.Api.Models;
+using StroiTrendTask.Api.Services;
 
 namespace StroiTrendTask.Api.Controllers;
 
@@ -6,5 +8,37 @@ namespace StroiTrendTask.Api.Controllers;
 [Route("api/[controller]")]
 public class ResponseTimeController : ControllerBase
 {
-    
+    private readonly IResponseTimeService _responseTimeService;
+
+    public ResponseTimeController(IResponseTimeService responseTimeService)
+    {
+        _responseTimeService = responseTimeService;
+    }
+        
+    /// <summary>
+    /// Gets the response time data.
+    /// </summary>
+    /// <returns>A json response with response time data.</returns>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     GET /api/responsetime
+    ///     
+    /// </remarks>
+    /// <response code="200">Returns a json response with response time data</response>
+    /// <response code="404">If the resource could not be found</response>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ResponseTime>> GetResponseTime()
+    {
+        var responseTime = await _responseTimeService.GetData();
+
+        if (responseTime is null)
+        {
+            return NotFound("Response time could not be found");
+        }
+
+        return Ok(responseTime);
+    }
 }
